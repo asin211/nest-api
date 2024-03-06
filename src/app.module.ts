6fed53d13b8f6626488f3bf48a837dd51a533dec
amrinder <asin211@mywhitecliffe.com>
2024-03-06 12:54:30 +1100

    fixed deleted imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,9 @@ import { UsersModule } from './users/users.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}), MongooseModule.forRoot(process.env.DATABASE_URL), EmployeesModule ],
+  imports: [UsersModule, ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}), MongooseModule.forRoot(process.env.DATABASE_URL), ],
   controllers: [AppController],
   providers: [AppService],
 })
